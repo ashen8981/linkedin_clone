0586eff397fa435448399c75a98364a859cb7945
Ashen Malindu <99719748+ashen8981@users.noreply.github.com>
2024-06-21 22:37:34 +0530

    new colors

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -6,6 +6,13 @@ const Color dbCustomColor = Color.fromRGBO(8, 119, 178, 1.0);
 const Color dlbCustomColor = Color.fromRGBO(214, 233, 242, 1.0);
 const Color gwCustomColor = Color.fromRGBO(184, 184, 184, 1.0);
 const Color bgCustomColor = Color.fromRGBO(17, 79, 90, 1.0);
-const Color black = Colors.black;
+const Color bg1Color = Color(0xFF608A91);
+const Color blackB = Colors.black;
 const Color greyB = Colors.grey;
+const Color iconPPColor = Color.fromRGBO(189, 215, 227, 1.0);
+const Color grey1 = Color(0xFF98989D);
+const commonBlue = Colors.blue;
+const Color greyL = Color.fromRGBO(232, 230, 224, 1.0);
+const Color blueG = Color.fromRGBO(219, 230, 240, 1.0);
+
 
